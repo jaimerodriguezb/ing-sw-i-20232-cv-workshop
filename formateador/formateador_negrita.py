feab6b69da15b27eed4e7e3f4431bf80eb93b4d3
Santiago <santiago.gutierrezyg@gmail.com>
2023-10-12 21:06:53 -0500

    Negrita 1.0

--- formateador/formateador_negrita.py
+++ formateador/formateador_negrita.py
@@ -1,4 +1,7 @@
 from comando_formateador import FormateadorComando
 
 class Negrita(FormateadorComando):
-    pass
+    def __init__(self, texto) -> None:
+        super().__init__(texto)
+    def execute(self):
+        return "*{}*".format(self.texto)
