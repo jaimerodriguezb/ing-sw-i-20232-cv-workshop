e7d8693ceb894d99c59857381d09793fd34f4341
maicol Ascencio <mascencio63@uan.edu.co>
2023-10-24 15:44:35 -0500

    Deshacer

--- formateador/formateador_subrayo.py
+++ formateador/formateador_subrayo.py
@@ -2,4 +2,4 @@ from comando_formateador import FormateadorComando
 
 class Subrayo(FormateadorComando):
     def execute(self):
-        return "*~~*".format(self.texto)
+        return "~"+self.texto+"~"
